1ed12b9a30e2b73aa9b4abd29274ede493f8d87f
Andrew Yasso <andrew.yasso@gmail.com>
2018-05-26 19:37:14 -0600

    add js drill

--- JavaScript/arraynumber.js
+++ JavaScript/arraynumber.js
@@ -1,3 +1,19 @@
 // Write a script that takes in an array and a number and returns only the elements larger than the specified number.
+var array = []
+for (var i = 1; i <= 100; i++) {
+   array.push(i);
+}
 
-function 
+
+function arraynum(array, num){
+  var output = []
+  var num = num
+  for (var x in array){
+    if (x > num){
+      output.push(x)
+    }
+  }
+  console.log(output)
+}
+
+console.log(arraynum(array, 50))
